c5d399b885908ce0b82226f585f43555dc458c33
zhebiewang <fwangjie@gmail.com>
2014-07-25 21:17:58 +0800

    change category

--- screenautolock/src/capture/CameraCapture.java
+++ screenautolock/src/capture/CameraCapture.java
@@ -2,31 +2,39 @@ package capture;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 
 import detection.FaceDetection;
 
-
 public class CameraCapture {
 
-
 	public static void main(String[] args) throws Exception {
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
 		VideoCapture capture;
 		Mat frame = new Mat();
-		
+		// Highgui highgui = new Highgui();
+
 		capture = new VideoCapture(0);
-		
-		if(!capture.isOpened()){
-			System.out.println("Did not connect to camera.");		
+		capture.open(0);
+
+		int N = 100;
+
+		for (int i = 1; i < N; i++) {
+			if (!capture.isOpened()) {
+				System.out.println("Did not connect to camera.");
+			} else {
+				capture.read(frame);
+				// String fileName = "C:\\New_Thinking\\Frame\\frame"+i+".jpg";
+				String fileName = "./pic/"+i + ".jpg";
+				System.out.println(fileName);
+				Highgui.imwrite(fileName, frame);
+				// FaceDetection.detection(frame);
+			}
 		}
-		else{
-			capture.retrieve(frame);
-			FaceDetection.detection(frame);
-			capture.release();
-		}
-		
+		capture.release();
+		new FaceDetection().run();
 	}
 
 }
--- screenautolock/src/detection/FaceDetection.java
+++ screenautolock/src/detection/FaceDetection.java
@@ -1,9 +1,44 @@
 package detection;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class FaceDetection {
-	public static void detection(Mat frame){
-		
+
+	public void run() {
+		System.out.println("\nRunning DetectFaceDemo");
+
+		// Create a face detector from the cascade file in the resources
+		// directory.
+		String pic_path = "./pic/80.jpg";
+		String xml_path = "./conf/haarcascade_frontalface_alt.xml";
+
+		CascadeClassifier faceDetector = new CascadeClassifier(xml_path);
+		Mat image = Highgui.imread(pic_path);
+
+		// Detect faces in the image.
+		// MatOfRect is a special container class for Rect.
+		MatOfRect faceDetections = new MatOfRect();
+		faceDetector.detectMultiScale(image, faceDetections);
+
+		System.out.println(String.format("Detected %s faces",
+				faceDetections.toArray().length));
+
+		// Draw a bounding box around each face.
+		for (Rect rect : faceDetections.toArray()) {
+			Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x
+					+ rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+		}
+		// Save the visualized detection.
+		String filename = "./pic/result/result.png";
+		System.out.println(String.format("Writing %s", filename));
+		Highgui.imwrite(filename, image);
 	}
+
 }
